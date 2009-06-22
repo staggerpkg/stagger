1bc9fba5382998ab66835878f40f208fbb1e6639
Károly Lőrentey <karoly@lorentey.hu>
2009-06-22 02:40:18 +0000

    setup.py:  Promote to version 0.2.0, in alpha. __init__.py: Add version number.

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ from distutils.core import setup;
 
 setup(
     name="stagger",
-    version="0.1.0",
+    version="0.2.0",
     url="http://code.google.com/p/stagger",
     author="Karoly Lorentey",
     author_email="karoly@lorentey.hu",
     packages=["stagger"],
     description="ID3v1/ID3v2 tag manipulation package in pure Python 3",
     long_description="""
-The package is currently in pre-alpha stage, under active development.
+The package is currently in alpha stage, under active development.
 
 The ID3v2 tag format is notorious for its useless specification
 documents and its quirky, mutually incompatible
@@ -20,7 +20,7 @@ that is able to handle all the various badly formatted tags out there
 and allow you to convert them to a consensus format.
 """,
     classifiers=[
-        "Development Status :: 2 - Pre-Alpha",
+        "Development Status :: 3 - Alpha",
         "Programming Language :: Python :: 3",
         "Topic :: Multimedia :: Sound/Audio"
         ],
