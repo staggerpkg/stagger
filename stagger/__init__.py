8023a29a5da68474515ab6ebcfe9674eada8de01
Károly Lőrentey <karoly@lorentey.hu>
2009-06-29 18:53:28 +0000

    bin/stagger: Reorganize.  Implement --set, --remove, --verbose and --no-act.  Better error handling.
    stagger/util.py: New file.  (Utilities for the shell frontend.)
    
    stagger/__init__.py: Import it.  Add default_tag global variable.

--- stagger/__init__.py
+++ stagger/__init__.py
@@ -3,6 +3,7 @@
 import stagger.frames
 import stagger.tags
 import stagger.id3
+import stagger.util
 
 from stagger.errors import *
 from stagger.frames import Frame, ErrorFrame, UnknownFrame, TextFrame, URLFrame
@@ -10,3 +11,5 @@ from stagger.tags import read_tag, decode_tag, delete_tag, Tag22, Tag23, Tag24
 from stagger.id3v1 import Tag1
 
 version = (0, 2, 0)
+
+default_tag = Tag24
