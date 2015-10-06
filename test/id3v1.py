b325422ed6a910ba0598e4a9e24a0961bb1a882d
Karoly Lorentey <karoly@lorentey.hu>
2015-10-06 22:28:29 +0200

    Update URL to id3v1 test suite.

--- test/id3v1.py
+++ test/id3v1.py
@@ -73,7 +73,8 @@ import warnings
 from stagger.errors import *
 import stagger.id3v1
 
-testsuite_url = r"http://www.id3.org/Developer_Information?action=AttachFile&do=get&target=id3v1_test_suite.tar.gz"
+                 
+testsuite_url = r"http://id3.org/Developer%20Information?action=AttachFile&do=get&target=id3v1_test_suite.tar.gz"
 testsuite_file = os.path.join(os.path.dirname(__file__), "id3v1_test_suite.tar.gz")
 
 testsuite_log = "id3v1/generation.log"
